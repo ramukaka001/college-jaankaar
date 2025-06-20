e7d1d59a75a5a282f551d0391ad05413232bf46e
Ravikant Saini <ravikantsaini047@gmail.com>
2025-06-20 19:55:52 +0530

    fix: Update site title in index.html and correct Razorpay key in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = "https://api.collegejaankaar.in/api";
 
-export const RAZORPAY_KEY = "rzp_live_jmnZsPwScIagXm";
+export const RAZORPAY_KEY = "rzp_live_J4DqHO6NQAbAnz";
 
 export const SITE = {
     name: 'College',
